73d0ec64951e3f9f9a1607d72bf7dc8cbb104e15
chuongvo0310 <chuongvh310@gmail.com>
2020-04-24 15:17:04 +0700

    login page

---
+++ src/Presentation/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Factories.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+		IUnitOfWork _unitOfWork;
+		public HomeController(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ src/Presentation/Controllers/AccountController.cs
@@ -0,0 +1,60 @@
+﻿using Factories.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Models.Identity;
+using Models.ViewModels.Account;
+using Presentation.Cores;
+using System.Threading.Tasks;
+
+namespace Presentation.Controllers
+{
+	public class AccountController : ControllerBaseTemplate
+	{
+		private SignInManager<AppUser> _signInManager;
+		private UserManager<AppUser> _userManager;
+
+		public AccountController(SignInManager<AppUser> signInManager, UserManager<AppUser> userManager)
+		{
+			_signInManager = signInManager;
+			_userManager = userManager;
+		}
+
+		[HttpGet]
+		public IActionResult Login()
+		{
+			return View();
+		}
+		[HttpPost]
+		public async Task<IActionResult> Login(LoginViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				var user = await _userManager.FindByEmailAsync(model.Email);
+				if (user != null && user.IsDisable == 0)
+				{
+					var result = await _signInManager.PasswordSignInAsync(user, model.Password, false, false);
+					if (result.Succeeded)
+					{
+						if (user.IsAdmin == 1)
+						{
+							return RedirectToAction("Index", "Home", new { Area = "Admin" });
+						}
+						if (user.IsAdmin == 0)
+						{
+							return RedirectToAction("Index", "Home");
+						}
+					}
+					else
+					{
+						ModelState.AddModelError(nameof(model.Password), "Mật khẩu không đúng");
+					}
+				}
+				else
+				{
+					ModelState.AddModelError("", "Tài khoản không tồn tại hoặc đã bị khóa");
+				}
+			}
+			return View(model);
+		}
+	}
+}
